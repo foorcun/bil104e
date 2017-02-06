c3fca52accf2b739779b5d12fb744ee4d34a605e
foorcun <msnifoorcun@hotmail.com>
2017-02-06 09:03:11 +0200

    Create begin11.c

---
+++ begin11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    
+    /*variables*/
+    int a=5; int b=3; int  area;
+    /*executeble  states*/
+    
+    area = a*b;
+    printf("%d\n", area);
+    
+    system("pause");
+    
+    return 0;
+    
+    
+    
+    }
