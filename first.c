ebc00f4ad54bdb4e21c7946e0371ccc9ece966d6
foorcun <msnifoorcun@hotmail.com>
2017-02-06 09:03:01 +0200

    Create first.c

---
+++ first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* the first c program*/
+
+int main()
+{
+    printf("welcome to c!\n");
+     printf("hosbulduk!\n");
+    
+    system("pause");  /* sacede c de �al���r*/
+    return 0;
+    
+    }
