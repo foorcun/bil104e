beb90a33196a231df8b31be9dba711c68ff5abae
foorcun <msnifoorcun@hotmail.com>
2017-02-27 09:05:27 +0200

    Add files via upload

---
+++ while_loop.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    int i=1;
+    
+    while(i<=10)
+    {
+                printf("hello\n");
+                //i=i+1;
+                i++;
+                
+                }
+   
+   
+    system("PAUSE");
+    return 0;
+    
+    }
